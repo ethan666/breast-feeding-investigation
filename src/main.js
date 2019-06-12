7097da20d43118afb87b6e20df879763d452bdb1
tanyiyang <690167690@qq.com>
2019-06-12 23:21:55 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import {
+  Input, 
+  Button, 
+  Select, 
+  Card, 
+  Form, 
+  Row, 
+  Col, 
+  Modal, 
+  Table, 
+  Checkbox,
+  Icon,
+  notification
+} from 'ant-design-vue'
+
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Card)
+Vue.use(Form)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Modal)
+Vue.use(Table)
+Vue.use(Checkbox)
+Vue.use(Icon)
+Vue.use(notification)
 
 Vue.config.productionTip = false;
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,97 @@
 <template>
-    <div></div>
+  <a-form
+    :form="form"
+    class="login-form"
+    @submit="handleSubmit"
+  >
+    <a-form-item>
+      <a-input
+        v-decorator="[
+          'userName',
+          { rules: [{ required: true, message: '请输入用户名！' }] }
+        ]"
+        placeholder="用户名"
+      >
+        <a-icon
+          slot="prefix"
+          type="user"
+          style="color: rgba(0,0,0,.25)"
+        />
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-input
+        v-decorator="[
+          'password',
+          { rules: [{ required: true, message: '请输入密码！' }] }
+        ]"
+        type="password"
+        placeholder="密码"
+      >
+        <a-icon
+          slot="prefix"
+          type="lock"
+          style="color: rgba(0,0,0,.25)"
+        />
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-checkbox
+        v-decorator="[
+          'remember',
+          {
+            valuePropName: 'checked',
+            initialValue: true,
+          }
+        ]"
+      >
+        记住我
+      </a-checkbox>
+      <a-button
+        type="primary"
+        html-type="submit"
+        class="login-form-button"
+      >
+        登录
+      </a-button>
+    </a-form-item>
+  </a-form>
 </template>
 
-<style lang="sass" scoped>
-    
-</style>
+<script>
 
+export default {
+  beforeCreate () {
+    this.form = this.$form.createForm(this);
+  },
+  methods: {
+    handleSubmit (e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login-form{
+    max-width: 300px;
+    height: 200px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+}
+.login-form-forgot {
+    float: right;
+}
+.login-form-button {
+    width: 100%;
+}
+</style>
